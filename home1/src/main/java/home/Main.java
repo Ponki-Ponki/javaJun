12b5221470a01604f4f8e82124769abdbe6ba5b4
Юрий Мусихин <vensop@gmail.com>
2023-12-06 20:32:25 +0300

    update

--- home1/src/main/java/home/Main.java
+++ home1/src/main/java/home/Main.java
@@ -1,7 +1,14 @@
 package home;
 
+import java.util.Arrays;
+import java.util.List;
+
+
 public class Main {
     public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(2,3,4,5,324,5,234,5,324,5,234,452,246,345,6,456);
+        System.out.println(list);
+        System.out.println("Среднее значение чётных чисел : " + list.stream().filter(i -> (i % 2) == 0).mapToInt(e -> e).average().getAsDouble());
 
     }
 }
